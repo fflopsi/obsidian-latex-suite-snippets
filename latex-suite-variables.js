fd2335678e1662684e4fff16332e4f2362a7059c
fflopsi <florian.l.frauenfelder@gmail.com>
2024-10-17 22:40:09 +0200

    Update variables, add trig

--- latex-suite-variables.js
+++ latex-suite-variables.js
@@ -1,5 +1,9 @@
 {
-	"${GREEK}": "alpha|beta|gamma|Gamma|delta|Delta|epsilon|varepsilon|zeta|eta|theta|vartheta|Theta|iota|kappa|lambda|Lambda|mu|nu|xi|omicron|pi|rho|varrho|sigma|Sigma|tau|upsilon|Upsilon|phi|varphi|Phi|chi|psi|omega|Omega",
-	"${SYMBOL}": "parallel|perp|partial|nabla|hbar|ell|infty|oplus|ominus|otimes|oslash|square|star|dagger|vee|wedge|subseteq|subset|supseteq|supset|emptyset|exists|nexists|forall|implies|impliedby|iff|setminus|neg|lor|land|bigcup|bigcap|cdot|times|simeq|approx",
-	"${MORE_SYMBOLS}": "leq|geq|neq|gg|ll|equiv|sim|propto|rightarrow|leftarrow|Rightarrow|Leftarrow|leftrightarrow|to|mapsto|cap|cup|in|sum|prod|exp|ln|log|det|dots|vdots|ddots|pm|mp|int|iint|iiint|oint"
+"${GREEK}": "alpha|beta|gamma|Gamma|delta|Delta|epsilon|varepsilon|zeta|eta|theta|vartheta|Theta|iota|kappa|lambda|Lambda|mu|nu|xi|Xi|omicron|pi|Pi|rho|varrho|sigma|Sigma|tau|upsilon|Upsilon|phi|varphi|Phi|chi|psi|Psi|omega|Omega",
+"${SYMBOL}": "hbar|ell|nabla|infty|dots|cdots|ddots|leftrightarrow|mapsto|setminus|mid|bigcap|bigcup|cap|cup|land|lor|subseteq|subset|implies|impliedby|iff|exists|forall|square|neq|geq|leq|gg|ll|sim|simeq|approx|equiv|cong|propto|cdot|oplus|ominus|otimes|times|star|perp|det|exp|ln|log|partial|neg|quad|qquad|dim|circ|arg|max|min|sup|inf|sum|prod|ker",
+"${SHORT_SYMBOL}": "to|pm|mp",
+"${TRIG}": "sin|cos|tan|cot|sec|csc|arcsin|arccos|arctan",
+"${TRIG_NEW}": "arccot|arcsec|arccsc",
+"${HYP_TRIG}": "sinh|cosh|tanh|coth",
+"${HYP_TRIG_NEW}": "sech|csch",
 }
