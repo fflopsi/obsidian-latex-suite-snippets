3cae2f0946003cbe7044f84b4d570c5a284f9ebc
fflopsi <florian.l.frauenfelder@gmail.com>
2024-11-14 23:42:08 +0100

    Add imath and jmath as greek-like

--- latex-suite-snippets.js
+++ latex-suite-snippets.js
@@ -344,7 +344,6 @@ export default [
 //{trigger: /([abcfgu-z])nk/, replacement: "[[0]]_{n_{k}}", options: "mA"},
 {trigger: /-1([i-n])/, replacement: (s) => `(-1)^{${l(s[1])}}`, options: "mA"},
 
-//{trigger: /inc/, replacement: "\\imath", options: "mA"},
 {trigger: /([NZQRC])\1/, replacement: "\\mathbb{[[0]]}", options: "mA"},
 {trigger: /\\mathbb\{([NZQRC])\}([2-9a-z+-])/, replacement: "\\mathbb{[[0]]}^{[[1]]}", options: "mA"},
 {trigger: /\\mathbb\{N\}0/, replacement: "\\mathbb{N}_{0}", options: "mA"},
