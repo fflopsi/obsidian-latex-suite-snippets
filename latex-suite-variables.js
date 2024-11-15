c4e9fb65338176c72c9adb70f86a5f1b5207bbd1
fflopsi <florian.l.frauenfelder@gmail.com>
2024-11-15 20:01:00 +0100

    Reorder #5

--- latex-suite-variables.js
+++ latex-suite-variables.js
@@ -1,11 +1,11 @@
 {
 GREEK: "alpha|beta|gamma|Gamma|delta|Delta|epsilon|varepsilon|zeta|eta|theta|vartheta|Theta|iota|kappa|lambda|Lambda|mu|nu|xi|Xi|omicron|pi|Pi|rho|varrho|sigma|Sigma|tau|upsilon|Upsilon|phi|varphi|Phi|chi|psi|Psi|omega|Omega|ell|hbar|partial|imath|jmath",
+DECO: "bar|check|dot|ddot|hat|mathring|tilde|vec|overline",
 FUNCTION: "exp|ln|log|dim|ker|arg",
-SYMBOL: "exp|ln|log|dim|ker|arg|nabla|infty|dots|cdots|ddots|leftrightarrow|mapsto|setminus|mid|bigcap|bigcup|cap|cup|land|lor|subseteq|subset|implies|impliedby|iff|exists|forall|square|neq|geq|leq|gg|ll|sim|simeq|approx|equiv|cong|propto|cdot|oplus|ominus|odot|otimes|oslash|times|star|perp|det|neg|quad|qquad|circ|max|min|sup|inf|sum|prod|notin",
-SHORT_SYMBOL: "to|pm|mp|in|ni",
 TRIG: "sin|cos|tan|cot|sec|csc|arcsin|arccos|arctan",
 TRIG_NEW: "arccot|arcsec|arccsc",
 HYP_TRIG: "sinh|cosh|tanh|coth",
 HYP_TRIG_NEW: "sech|csch",
-DECO: "bar|check|dot|ddot|hat|mathring|tilde|vec|overline",
+SYMBOL: "exp|ln|log|dim|ker|arg|nabla|infty|dots|cdots|ddots|leftrightarrow|mapsto|setminus|mid|bigcap|bigcup|cap|cup|land|lor|subseteq|subset|implies|impliedby|iff|exists|forall|square|neq|geq|leq|gg|ll|sim|simeq|approx|equiv|cong|propto|cdot|oplus|ominus|odot|otimes|oslash|times|star|perp|det|neg|quad|qquad|circ|max|min|sup|inf|sum|prod|notin",
+SHORT_SYMBOL: "to|pm|mp|in|ni",
 }
