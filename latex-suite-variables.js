40de83142c087d87c69b9353a03b6c80e08e8cf6
fflopsi <florian.l.frauenfelder@gmail.com>
2024-11-15 19:51:57 +0100

    Add hbar to greek

--- latex-suite-variables.js
+++ latex-suite-variables.js
@@ -1,6 +1,6 @@
 {
-GREEK: "alpha|beta|gamma|Gamma|delta|Delta|epsilon|varepsilon|zeta|eta|theta|vartheta|Theta|iota|kappa|lambda|Lambda|mu|nu|xi|Xi|omicron|pi|Pi|rho|varrho|sigma|Sigma|tau|upsilon|Upsilon|phi|varphi|Phi|chi|psi|Psi|omega|Omega|ell|partial|imath|jmath",
 SYMBOL: "hbar|nabla|infty|dots|cdots|ddots|leftrightarrow|mapsto|setminus|mid|bigcap|bigcup|cap|cup|land|lor|subseteq|subset|implies|impliedby|iff|exists|forall|square|neq|geq|leq|gg|ll|sim|simeq|approx|equiv|cong|propto|cdot|oplus|ominus|odot|otimes|oslash|times|star|perp|det|exp|ln|log|neg|quad|qquad|dim|circ|arg|max|min|sup|inf|sum|prod|ker|notin",
+GREEK: "alpha|beta|gamma|Gamma|delta|Delta|epsilon|varepsilon|zeta|eta|theta|vartheta|Theta|iota|kappa|lambda|Lambda|mu|nu|xi|Xi|omicron|pi|Pi|rho|varrho|sigma|Sigma|tau|upsilon|Upsilon|phi|varphi|Phi|chi|psi|Psi|omega|Omega|ell|hbar|partial|imath|jmath",
 FUNCTION: "exp|ln|log|dim|ker|arg",
 SHORT_SYMBOL: "to|pm|mp|in|ni",
 TRIG: "sin|cos|tan|cot|sec|csc|arcsin|arccos|arctan",
