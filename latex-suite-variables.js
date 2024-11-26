e94a396e565365554911900079223a0479da797e
fflopsi <florian.l.frauenfelder@gmail.com>
2024-11-26 13:55:20 +0100

    Add arrows, move nabla to greek, reorder #1 #2

--- latex-suite-variables.js
+++ latex-suite-variables.js
@@ -1,11 +1,11 @@
 {
-GREEK: "alpha|beta|gamma|Gamma|delta|Delta|epsilon|varepsilon|zeta|eta|theta|vartheta|Theta|iota|kappa|lambda|Lambda|mu|nu|xi|Xi|omicron|pi|Pi|rho|varrho|sigma|Sigma|tau|upsilon|Upsilon|phi|varphi|Phi|chi|psi|Psi|omega|Omega|ell|hbar|partial|imath|jmath",
+GREEK: "alpha|beta|gamma|Gamma|delta|Delta|epsilon|varepsilon|zeta|eta|theta|vartheta|Theta|iota|kappa|lambda|Lambda|mu|nu|xi|Xi|omicron|pi|Pi|rho|varrho|sigma|Sigma|tau|upsilon|Upsilon|phi|varphi|Phi|chi|psi|Psi|omega|Omega|ell|hbar|partial|nabla|imath|jmath",
 DECO: "bar|check|dot|ddot|hat|mathring|tilde|vec|overline",
 FUNCTION: "exp|ln|log|dim|ker|arg",
 TRIG: "sin|cos|tan|cot|sec|csc|arcsin|arccos|arctan",
 TRIG_NEW: "arccot|arcsec|arccsc",
 HYP_TRIG: "sinh|cosh|tanh|coth",
 HYP_TRIG_NEW: "sech|csch",
-SYMBOL: "exp|ln|log|dim|ker|arg|nabla|infty|dots|cdots|ddots|leftrightarrow|mapsto|setminus|mid|bigcap|bigcup|cap|cup|land|lor|subseteq|subset|implies|impliedby|iff|exists|forall|square|neq|geq|leq|gg|ll|sim|simeq|approx|equiv|cong|propto|cdot|oplus|ominus|odot|otimes|oslash|times|star|perp|det|neg|quad|qquad|circ|max|min|sup|inf|sum|prod|notin",
+SYMBOL: "exp|ln|log|dim|ker|arg|det|infty|dots|cdots|ddots|vdots|uparrow|downarrow|updownarrow|leftarrow|rightarrow|leftrightarrow|nearrow|nwarrow|searrow|swarrow|mapsto|setminus|mid|cap|cup|bigcap|bigcup|land|lor|subset|subseteq|subsetneq|supset|supseteq|supsetneq|implies|impliedby|iff|exists|forall|square|neq|leq|geq|ll|gg|sim|simeq|approx|equiv|cong|propto|cdot|oplus|ominus|odot|otimes|oslash|times|star|perp|neg|quad|qquad|circ|max|min|sup|inf|sum|prod|notin",
 SHORT_SYMBOL: "to|pm|mp|in|ni",
 }
